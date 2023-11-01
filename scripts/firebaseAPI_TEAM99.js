3574a3d786bc367194d752c7fe64ce9ca0a788bd
Reza Khoddammohammadi <rkhoddammohammadi@my.bcit.ca>
2023-10-31 20:18:55 -0700

    message

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyB8bpqsFX7JS8RUUdHeOWDfU7Fzv2v9ZE4",
+  authDomain: "comp1800-7831e.firebaseapp.com",
+  projectId: "comp1800-7831e",
+  storageBucket: "comp1800-7831e.appspot.com",
+  messagingSenderId: "651951442983",
+  appId: "1:651951442983:web:4c899638f96b04d41a9202"
 };
 
 //--------------------------------------------

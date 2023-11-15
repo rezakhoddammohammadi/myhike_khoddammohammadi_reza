55c5ef3dada85de48aff2bbfef73800d0cd5a0ce
Reza Khoddammohammadi <rkhoddammohammadi@my.bcit.ca>
2023-11-15 11:14:09 -0800

    all changes updated

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -3,11 +3,11 @@
 //----------------------------------------
 var firebaseConfig = {
     apiKey: "AIzaSyB8bpqsFX7JS8RUUdHeOWDfU7Fzv2v9ZE4",
-    authDomain: "comp1800-7831e.firebaseapp.com",
-    projectId: "comp1800-7831e",
-    storageBucket: "comp1800-7831e.appspot.com",
-    messagingSenderId: "651951442983",
-    appId: "1:651951442983:web:4c899638f96b04d41a9202"
+  authDomain: "comp1800-7831e.firebaseapp.com",
+  projectId: "comp1800-7831e",
+  storageBucket: "comp1800-7831e.appspot.com",
+  messagingSenderId: "651951442983",
+  appId: "1:651951442983:web:4c899638f96b04d41a9202"
 };
 
 //--------------------------------------------
--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,52 @@ function sayHello() {
     //do something
 }
 //sayHello();    //invoke function
+
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
+
+
+
+
+
+
+//----------------------------------------------------------
+// This function is the only function that's called.
+// This strategy gives us better control of the page.
+//----------------------------------------------------------
+function doAll() {
+  firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+          insertNameFromFirestore(user);
+          getBookmarks(user)
+      } else {
+          console.log("No user is signed in");
+      }
+  });
+}
+doAll();
+
+//----------------------------------------------------------
+// Wouldn't it be nice to see the User's Name on this page?
+// Let's do it!  (Thinking ahead:  This function can be carved out, 
+// and put into script.js for other pages to use as well).
+//----------------------------------------------------------//----------------------------------------------------------
+function insertNameFromFirestore(user) {
+          db.collection("users").doc(user.uid).get().then(userDoc => {
+              console.log(userDoc.data().name)
+              userName = userDoc.data().name;
+              console.log(userName)
+              document.getElementById("name-goes-here").innerHTML = userName;
+          })
+
+}
